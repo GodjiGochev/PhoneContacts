6fe4da1ec3ec2352a63ad84936a70da899319a6c
Grigor <godji1998@abv.bg>
2024-01-22 22:05:56 +0200

    New

--- PhoneContacts.java
+++ PhoneContacts.java
@@ -1,8 +1,8 @@
-package PhoneContacts;
-
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
-public class PhoneContacts {
+public class phoneContacts {
     public static void main(String[] args) {
         //Scanner so I can log in my new numbers
         Scanner scanner = new Scanner(System.in);
@@ -13,7 +13,26 @@ public class PhoneContacts {
         int inputPhoneNumber = Integer.parseInt(scanner.nextLine());
         //Title to enter a name
         System.out.print("Enter name: ");
-        String inputName = scanner.nextLine();
-        System.out.println(inputPhoneNumber);
+        String input = scanner.nextLine();
+        //LinkedHashMap where I will keep my phone numbers
+        LinkedHashMap<String, Integer> phoneNumberStored = new LinkedHashMap<>();
+        //New input for the loop
+        String command = scanner.nextLine();
+        //While loop for the command (input)
+        while (!command.equals("call")) {
+            switch (command) {
+                case "Add":
+                    phoneNumberStored.put(input, inputPhoneNumber);
+                    break;
+            }
+            command = scanner.nextLine();
+        }
+        for (Map.Entry<String, Integer> entry : phoneNumberStored.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println(key);
+            System.out.println(value);
+
+        }
     }
 }
